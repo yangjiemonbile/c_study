594a9a73dce4adccbe9d7b4e8e482678de8fcdd5
yangjie <1226033831@qq.com>
2019-01-12 18:03:30 +0800

    修改cmake参数

--- apue/log/syslog.c
+++ apue/log/syslog.c
@@ -1,61 +1,54 @@
-#include "apue.h"
+#include <include/apue.h>
 #include <errno.h>
 #include <stdarg.h>
 #include <syslog.h>
 
-static void log_doit(int,int,const char*,va_list ap);
+static void log_doit(int, int, const char *, va_list ap);
 
-extern int log_to_stderr;
-
-void 
-log_open(const char *ident,int option,int facility)
+void log_open(const char *ident, int option, int facility)
 {
 
 }
 
-void 
-log_ret(const char *fmt,...)
+void log_ret(const char *fmt, ...)
 {
     va_list ap;
 }
 
-void 
-log_sys(const char *fmt,...)
+void log_sys(const char *fmt, ...)
 {
 
 }
 
-void 
-log_msg(const char *fmt,...)
+void log_msg(const char *fmt, ...)
 {
 
 }
 
-void 
-log_quit(const char *fmt,...)
+void log_quit(const char *fmt, ...)
 {
-
+    
 }
 
 static void
-log_doit(int errnoflag,int priority,const char *fmt,va_list ap)
+log_doit(int errnoflag, int priority, const char *fmt, va_list ap)
 {
     int errno_save;
     char buf[MAXLINE];
 
     errno_save = errno;
-    vsnprintf(buf,MAXLINE,fmt,ap);
-    if(errnoflag)
-        snprintf(buf+strlen(buf),MAXLINE-strlen(buf),": %s",
-        strerror(errno_save));
+    vsnprintf(buf, MAXLINE, fmt, ap);
+    if (errnoflag)
+        snprintf(buf + strlen(buf), MAXLINE - strlen(buf), ": %s",
+                 strerror(errno_save));
 
-    strcat(buf,"\n");
-    if(log_to_stderr) {
-        fflush(stdout);
-        fputs(buf,stderr);
-        fflush(stderr);
-    } else {
-        syslog(priority,buf);
-    }
+    strcat(buf, "\n");
+#ifdef STD_ERROR
+    fflush(stdout);
+    fputs(buf, stderr);
+    fflush(stderr);
+#else
+    syslog(priority,fmt,buf);
+#endif
 }
 
