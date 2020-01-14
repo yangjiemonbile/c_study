8d1a2976dba395b7748a0175067f2976006b366e
jie <1226033831@qq.com>
2020-01-14 13:01:22 +0800

    修改代码结构

---
+++ cplus_study/src/0.template.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int isValid(std::vector<int> nums) {
+        return 1;
+    }
+};
+
+int main(int argc,char** argv)
+{
+    return EXIT_FAILURE;
+}
